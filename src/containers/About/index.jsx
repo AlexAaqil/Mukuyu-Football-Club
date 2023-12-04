596502c05e8bb49ba394463827719aac1c50b17f
alexaaqil <alexaaqil.se@gmail.com>
2023-12-04 20:08:34 +0300

    Updates for the home page

--- src/containers/About/index.jsx
+++ src/containers/About/index.jsx
@@ -1,25 +1,25 @@
-import "./about.scss"
+import "./about.scss";
 
 import images from "../../assets/images/images";
 
 export default function About() {
-    return (
-      <section id="about" className="About">
-        <div className="container">
-          <div className="text">
-            <h1>OUR STORY</h1>
-            <p>
-              Mukuyu Football Club’s story is utterly uniques in the football
-              sport, which is precisely why the club is revered and renowned all
-              over the country. It’s a head-spinning tale of triumphs, tragedy
-              and rich traditions often resulting in the kind of dramatic
-              endings usually reserved for movies. The club was founded in the
-              year 2004 and has ever since been a great club with honor and
-              distinction. Managers and teams have served in this great club.
-            </p>
-          </div>
-          <img src={ images.team_photo } alt="Team Photo" />
+  return (
+    <section id="about" className="About">
+      <div className="container">
+        <div className="text">
+          <h1>OUR STORY</h1>
+          <p>
+            Mukuyu Football Club’s story is utterly uniques in the football
+            sport, which is precisely why the club is revered and renowned all
+            over the country. It’s a head-spinning tale of triumphs, tragedy and
+            rich traditions often resulting in the kind of dramatic endings
+            usually reserved for movies. The club has been a great club with
+            honor and distinction. Managers and teams have served in this great
+            club.
+          </p>
         </div>
-      </section>
-    );
+        <img src={images.team_photo} alt="Team Photo" />
+      </div>
+    </section>
+  );
 }
