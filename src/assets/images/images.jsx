620ca19b2c9d8296e1818f16a824d3c111088665
alexaaqil <alexaaqil.se@gmail.com>
2023-12-04 13:09:17 +0300

    Styles and Images

---
+++ src/assets/images/images.jsx
@@ -0,0 +1,74 @@
+// General Images
+import logo from "./logo.jpeg";
+import table from "./table.jpeg";
+import team_photo from "./team photo.jpg";
+import image01 from "./01.jpeg";
+import image02 from "./02.jpeg";
+import image03 from "./03.jpeg";
+import image04 from "./04.jpeg";
+import image05 from "./05.jpeg";
+import image06 from "./06.jpeg";
+
+
+// Awards
+import award01 from "./award01.jpeg";
+import award02 from "./award02.jpeg";
+import award03 from "./award03.jpg";
+
+
+// Players
+import player1 from "./players/Player 1.jpeg"
+import player2 from "./players/Player 2.jpeg";
+import player3 from "./players/Player 3.jpeg";
+import player4 from "./players/Player 4.jpeg";
+import player5 from "./players/Player 5.jpeg";
+import player6 from "./players/Player 6.jpeg";
+import player7 from "./players/Player 7.jpeg";
+import player8 from "./players/Player 8.jpeg";
+import player9 from "./players/Player 9.jpeg";
+import player10 from "./players/Player 10.jpeg";
+import player11 from "./players/Player 11.jpeg";
+import player12 from "./players/Player 12.jpeg"
+import player13 from "./players/Player 13.jpeg";
+import player14 from "./players/Player 14.jpeg";
+import player15 from "./players/Player 15.jpeg";
+import player16 from "./players/Player 16.jpeg";
+import player17 from "./players/Player 17.jpeg";
+import player18 from "./players/Player 18.jpeg";
+
+const images = {
+  logo: logo,
+  table : table,
+  team_photo : team_photo,
+  image01: image01,
+  image02: image02,
+  image03: image03,
+  image04: image04,
+  image05: image05,
+  image06: image06,
+
+  award01: award01,
+  award02: award02,
+  award03: award03,
+
+  player1: player1,
+  player2: player2,
+  player3: player3,
+  player4: player4,
+  player5: player5,
+  player6: player6,
+  player7: player7,
+  player8: player8,
+  player9: player9,
+  player10: player10,
+  player11: player11,
+  player12: player12,
+  player13: player13,
+  player14: player14,
+  player15: player15,
+  player16: player16,
+  player17: player17,
+  player18: player18,
+};
+
+export default images;
