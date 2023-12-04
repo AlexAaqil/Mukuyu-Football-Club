ff0747963978e08ecf3d68257c926ab4db50b376
alexaaqil <alexaaqil.se@gmail.com>
2023-12-04 19:07:23 +0300

    Server configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import "./assets/css/styles.scss";
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./containers/home";
 import Fixtures from "./containers/fixtures";
-import Players from "./containers/players"
+import Players from "./containers/players";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/fixtures" element={ <Fixtures /> } />
-        <Route path="/players" element={ <Players /> } />
+        <Route path="/" element={<Home />} />
+        <Route path="/fixtures" element={<Fixtures />} />
+        <Route path="/players" element={<Players />} />
       </Routes>
     </Router>
   );
