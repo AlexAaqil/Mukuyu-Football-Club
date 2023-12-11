7317ccbc904261c6889dd04537c53aa6c3ac0aff
alexaaqil <alexaaqil.se@gmail.com>
2023-12-11 13:30:48 +0300

    Restructured the files and the routes on the App.jsx file to have parent and child routes for fixtures

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import "./assets/css/styles.scss";
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./containers/home";
-import Fixtures from "./containers/fixtures";
-import Players from "./containers/players";
+
+import Fixtures from "./containers/Fixtures/";
+import UpComingMatches from "./containers/Fixtures/UpcomingMatches";
+import MatchResults from "./containers/Fixtures/MatchResults";
+import Tables from "./containers/Fixtures/Tables";
+
+import Players from "./containers/Players";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/fixtures" element={<Fixtures />} />
+
+        <Route path="/fixtures" element={<Fixtures />}>
+          <Route index element={<UpComingMatches />} />
+          <Route path="results" element={<MatchResults />} />
+          <Route path="tables" element={<Tables />} />
+        </Route>
+
         <Route path="/players" element={<Players />} />
       </Routes>
     </Router>
